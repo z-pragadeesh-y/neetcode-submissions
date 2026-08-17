254df87b0244aebd242a7c4be3c2c903c9a13b77
z-pragadeesh-y <pragadeeshabi6@gmail.com>
2026-08-17 21:01:14 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        multiset<char>s1;
+        multiset<char>t1;
+        int n1=s.length();
+        int n2=t.length();
+        for(int i=0;i<n1;i++){
+            s1.insert(s[i]);
+        }
+        for(int i=0;i<n2;i++){
+            t1.insert(t[i]);
+        }
+        if(s1==t1){
+            return true;
+        }
+        return false;
+    }
+};
