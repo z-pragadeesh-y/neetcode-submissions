f490843a800d367f81fd7ed84713d364bee43331
z-pragadeesh-y <pragadeeshabi6@gmail.com>
2026-08-20 20:41:29 +0530

    Add: top-k-elements-in-list - submission-23

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-23.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        
+        map<int, int> mp;
+
+        for (int x : nums) {
+            mp[x]++;
+        }
+
+        // frequency → number
+        multimap<int, int> freq;
+
+        for (auto p : mp) {
+            freq.insert({p.second, p.first});
+        }
+
+        vector<int> ans;
+
+        int count = 0;
+
+        for (auto it = freq.rbegin(); it != freq.rend(); ++it) {
+
+            if (count == k)
+                break;
+
+            ans.push_back(it->second);
+            count++;
+        }
+
+        return ans;
+    }
+};
